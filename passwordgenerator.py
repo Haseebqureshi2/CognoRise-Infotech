e630820b6c530cf7c6b4f8bb30fec9335eb9ca77
Haseebqureshi2 <haseebqureshi5152@gmail.com>
2024-08-07 11:30:59 +0500

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,23 @@
+import string
+import random
+# Getting password length
+length = int(input("Enter password length: "))
+
+characterList = ""
+
+characterList += string.ascii_letters
+
+characterList += string.digits
+
+characterList += string.punctuation
+	
+
+password = []
+
+for i in range(length):
+	
+	randomchar = random.choice(characterList)
+
+	password.append(randomchar)
+
+print("The random password is " + "".join(password))
